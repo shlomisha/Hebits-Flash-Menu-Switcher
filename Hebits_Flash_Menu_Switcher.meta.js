d35533225befc582d85c539c62102f7be10ce6ad
Shlomi <shlomisha@users.noreply.github.com>
2015-08-08 15:19:41 +0300

    Create Hebits_Flash_Menu_Switcher.meta.js
    Creating the file.

---
+++ Hebits_Flash_Menu_Switcher.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @id             Hebits_Flash_Menu_Switcher
+// @name           Hebits Flash Menu Switcher
+// @version        1.0
+// @namespace      shlomisha
+// @author         shlomisha
+// @description    Changes the disgusting flash menu to clean HTML one
+// @include        https://hebits.net/*
+// @include        https://www.hebits.net/*
+// @grant          GM_addStyle
+// @run-at         document-end
+// @updateURL      https://raw.githubusercontent.com/shlomisha/Hebits-Flash-Menu-Switcher/master/Hebits_Flash_Menu_Switcher.meta.js
+// @downloadURL    https://raw.githubusercontent.com/shlomisha/Hebits-Flash-Menu-Switcher/master/Hebits_Flash_Menu_Switcher.user.js
+// ==/UserScript==
